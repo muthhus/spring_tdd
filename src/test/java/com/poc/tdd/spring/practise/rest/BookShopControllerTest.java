ae4681978cc7c13f2cde5026d9973cf5ef6ffc2d
Shan Sankara Pandian Muthu <shan.sankara-pandian-muthu@capgemini.com>
2018-11-02 20:36:46 +0000

    BookShopService init

--- src/test/java/com/poc/tdd/spring/practise/rest/BookShopControllerTest.java
+++ src/test/java/com/poc/tdd/spring/practise/rest/BookShopControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,6 +23,9 @@ public class BookShopControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private BookShopServive bookShopServive;
+
     @Test
     public void welcomeMessageTest() throws Exception{
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -33,4 +37,5 @@ public class BookShopControllerTest {
                             .andExpect(content().string("Hello, Welcome to world BookShop!"))
                             .andReturn();
     }
+    
 }
